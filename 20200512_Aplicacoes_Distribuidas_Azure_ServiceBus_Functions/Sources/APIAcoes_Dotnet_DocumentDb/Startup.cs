4dfa7143ab3298da3835ce5bf7c98e0b1d8eeba7
Gustavo Bellini Bigardi <gbbigardi@gmail.com>
2020-05-12 12:31:27 -0300

    adding api cotacoes

---
+++ 20200512_Aplicacoes_Distribuidas_Azure_ServiceBus_Functions/Sources/APIAcoes_Dotnet_DocumentDb/Startup.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace APIAcoes
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c => {
+
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Ações - Testes - Azure Service Bus",
+                        Version = "v1",
+                        Description = "Exemplo de API REST criada com o ASP.NET Core 3.1 para o envio de dados de ações a um Tópico do Azure Service Bus",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Renato Groffe",
+                            Url = new Uri("https://github.com/renatogroffe")
+                        }
+                    });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(builder => builder.AllowAnyMethod()
+                                          .AllowAnyOrigin()
+                                          .AllowAnyHeader());
+                                          
+            // Ativando middlewares para uso do Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ações - Testes - Azure Service Bus");
+            });
+
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
